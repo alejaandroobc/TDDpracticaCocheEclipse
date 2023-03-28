16affbc8e29d4df9e013c3fcc523e75b7d346778
AlejandroB <abotellacosta2@gmail.com>
2023-03-28 16:36:10 +0200

    Tercer test pasado con éxito

--- src/TestCoche.java
+++ src/TestCoche.java
@@ -5,10 +5,16 @@ import org.junit.jupiter.api.Test;
 public class TestCoche {
 
 	@Test
-	public void test_crear_coche() {
+	public void test_al_crear_un_coche_su_velocidad_es_cero() {
 		Coche nuevoCoche = new Coche();
 		Assertions.assertEquals(0, nuevoCoche.velocidad);
+	}
+	
+	@Test
+	public void test_al_crear_un_coche_su_velocidad_aumenta(){
+		Coche nuevoCoche = new Coche();
+		nuevoCoche.acelerar(30);
+		Assertions.assertEquals(30, nuevoCoche.velocidad);
 		
 	}
-
 }
