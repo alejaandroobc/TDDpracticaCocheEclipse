0e54a5b46b46197917f2a7c637ea71295a2e7096
AlejandroB <abotellacosta2@gmail.com>
2023-03-28 16:29:11 +0200

    Segundo test pasado con éxito

--- src/TestCoche.java
+++ src/TestCoche.java
@@ -1,12 +1,14 @@
-import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestCoche {
 
 	@Test
 	public void test_crear_coche() {
-		Coche nueviCoche = new Coche();
+		Coche nuevoCoche = new Coche();
+		Assertions.assertEquals(0, nuevoCoche.velocidad);
+		
 	}
 
 }
