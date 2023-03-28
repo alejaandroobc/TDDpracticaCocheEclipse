42f319b5f599f7dd0ebf4b3d49867ca440f47993
AlejandroB <abotellacosta2@gmail.com>
2023-03-28 16:55:38 +0200

    Se ha añadido la refactorización de la práctica

--- src/TestCoche.java
+++ src/TestCoche.java
@@ -11,14 +11,14 @@ public class TestCoche {
 	}
 	
 	@Test
-	public void test_al_acelerar_un_coche_su_velocidad_aumenta(){
+	public void test_al_acelerar_un_coche_su_velocidad_aumenta_AlejandroBotella(){
 		Coche nuevoCoche = new Coche();
 		nuevoCoche.acelerar(30);
 		Assertions.assertEquals(30, nuevoCoche.velocidad);
 		
 	}
 	@Test
-	public void test_al_decelerar_un_coche_su_velocidad_disminuye(){
+	public void test_al_decelerar_un_coche_su_velocidad_disminuye_AlejandroBotella(){
 		Coche nuevoCoche = new Coche();
 		nuevoCoche.velocidad= 50;
 		nuevoCoche.decelerar(20);
@@ -26,7 +26,7 @@ public class TestCoche {
 	}
 	
 	@Test
-	public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero(){
+	public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero_AlejandroBotella(){
 		Coche nuevoCoche = new Coche();
 		nuevoCoche.velocidad= 50;
 		nuevoCoche.decelerar(80);
